9e6bcf54b9043df24be5544887dcbef988902850
Matthias Bärwolff <matthias.baerwolff@epost-dev.de>
2015-09-30 01:31:04 +0200

    MK-4: decouple flip plugin from jquery

--- session4/src/js/flip.js
+++ session4/src/js/flip.js
@@ -1,22 +1,2 @@
 import $ from 'jquery';
-import { invert } from 'src/js/lib/invert';
-
-(function($) {
-
-    const Flipper = (element) => {
-        let scale = 1;
-        return () => {
-            scale = invert(scale);
-            element.css({
-                'transform': `scaleY(${scale})`
-            });
-        };
-    };
-    $.fn.clickFlip = function() {
-        return this.each(function() {
-            let element = $(this);
-            let flip = new Flipper(element);
-            element.click(flip);
-        });
-    };
-}($));
+import 'src/js/lib/flipPlugin';
---
+++ session4/src/js/lib/flipPlugin.js
@@ -0,0 +1,21 @@
+import { invert } from 'src/js/lib/invert';
+
+(function($) {
+
+  const Flipper = (element) => {
+    let scale = 1;
+    return () => {
+      scale = invert(scale);
+      element.css({
+        'transform': `scaleY(${scale})`
+    });
+  };
+};
+$.fn.clickFlip = function() {
+  return this.each(function() {
+    let element = $(this);
+    let flip = new Flipper(element);
+    element.on('click', flip);
+  });
+};
+}($));
